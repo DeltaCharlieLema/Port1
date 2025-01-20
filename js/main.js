c7c31ab7d3f079b52cfd9d524b2de38eae2af280
Delta Lema <180664295+DeltaCharlieLema@users.noreply.github.com>
2025-01-19 20:10:29 -0600

    changed index to have an image gallery for projects-work in progress

--- js/main.js
+++ js/main.js
@@ -31,12 +31,20 @@ class Movie extends Media {
         super(title);
         this._director = director;
         this._runTime = runTine;
+        this._movieCast = [];
     }
     get director(){
         return this._director;
     }
     get runTime(){
         return this._runTime;
+    }
+    get movieCast(){
+        return this._movieCast
+    }
+    addCastMember(member){
+        this._movieCast.push(member);
+        return this._movieCast;
     }
 }
 class Book extends Media {
@@ -66,8 +74,5 @@ class Cd extends Media {
     }
 
 }
-const robot = new Movie('John','The good robot',8);
-robot.addRating(3);
-robot.addRating(3);
-robot.addRating(10);
-console.log(robot.getAverageRating());
+const historyOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything',544);
+const speed = new Movie('Jan de Bont', 'speed', 116);
